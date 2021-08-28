5cf0b55954261e6dce722010a2bb2ba75b652b80
yws710 <yws710@163.com>
2021-08-28 11:59:56 +0800

    添加入口类注释

--- src/main/java/com/yws/jwtdemo/JwtdemoApplication.java
+++ src/main/java/com/yws/jwtdemo/JwtdemoApplication.java
@@ -3,11 +3,12 @@ package com.yws.jwtdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目入口
+ */
 @SpringBootApplication
 public class JwtdemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JwtdemoApplication.class, args);
 	}
-
 }
